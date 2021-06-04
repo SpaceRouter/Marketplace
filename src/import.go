e0659e97b7a519a49395d9665094604739910f89
Louis Desplanche <louis.desplanche@edu.esiee.fr>
2021-06-04 18:27:35 +0200

    Add information to import

--- src/import.go
+++ src/import.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/spacerouter/marketplace/models"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 	"gorm.io/gorm"
 	"io/ioutil"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 )
 
 func ImportCompose(path string, db *gorm.DB) {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Name:  ")
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Description:  ")
+	desc, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Icon:  ")
+	icon, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	file, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -28,6 +51,10 @@ func ImportCompose(path string, db *gorm.DB) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	stack.Name = name
+	stack.Description = desc
+	stack.Icon = icon
 
 	db.Create(stack)
 
